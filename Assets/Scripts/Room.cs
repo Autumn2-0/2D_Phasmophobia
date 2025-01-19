09d58871e55b708126f32f098ae5c7a9858e7fe2
simon <spinnoc@gmu.edu>
2025-01-19 14:42:17 -0500

    Add EMF and Temperature values to the rooms

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -6,12 +6,48 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public int EMF = 1;
     public int temperature = 65;
 
+    public GameObject lights;
+
+    //Add to the EMF value for the room
+    public void ChangeEMF(int amount)
+    {
+        EMF += amount;
+
+        EMF = Mathf.Clamp(EMF, 1, 5);
+    }
+
+    //Add to the Temperature value for the room
     public void ChangeTemperature(int amount)
     {
         temperature += amount;
 
         temperature = Mathf.Clamp(temperature, 15, 65);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            lights.SetActive(true);
+        }
+        else if (collision.CompareTag("Ghost"))
+        {
+            //Add the logic for changing the temperature and EMF based on the ghost
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            lights.SetActive(false);
+        }
+        else if (collision.CompareTag("Ghost"))
+        {
+            //Add the logic for changing the temperature and EMF based on the ghost
+        }
     }
 }
