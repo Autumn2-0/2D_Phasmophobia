d9caa0a4fdfa2c54c69a6c8a91fb5133b4dfeda7
simon <spinnoc@gmu.edu>
2025-01-19 17:15:40 -0500

    Added framework for ghost behaviour

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GhostType
+{
+    
+}
+
+public enum Evidence
+{
+
+}
+
 public class Ghost : MonoBehaviour
 {
     public bool FreezingTemps;
@@ -19,4 +29,50 @@ public class Ghost : MonoBehaviour
     public bool activeUV;
     public bool activeDots;
     public bool activeMS;
+
+    [Header("Area")]
+    public Room ghostRoom;
+    public Room currentRoom;
+    public int temperatureModifier = -25;
+
+    [Header("Movement")]
+    public float roamingSpeed = 3.5f;
+    public float huntingSpeed = 7f;
+    public bool hunting;
+    public float huntDuration;
+
+
+    private void Update()
+    {
+        Vector2 dirToPlayer = (transform.position - GameManager.player.transform.position);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToPlayer);
+
+        if (hit.collider.CompareTag("Player"))
+        {
+            StartCoroutine(HuntSequence());
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Room"))
+        {
+            currentRoom = collision.GetComponent<Room>();
+        }
+    }
+
+    public IEnumerator HuntSequence()
+    {
+        hunting = true;
+
+        //Change to hunting behaviour
+
+        yield return new WaitForSeconds(huntDuration);
+
+        //Change to Roaming behaviour
+
+        hunting = false;
+        ghostRoom = currentRoom;
+    }
 }
