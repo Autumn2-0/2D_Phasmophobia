ff4d052da5ed4baff2a954b262ac3cf54869b48d
alevy9-Birch <115123998+alevy9-Birch@users.noreply.github.com>
2025-01-19 18:14:24 -0500

    Item Update

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public abstract class Item : MonoBehaviour
@@ -12,17 +13,47 @@ public abstract class Item : MonoBehaviour
     protected float throwForce = 2f;
     protected bool canPlace = true;
     protected bool held = false;
+    protected bool pocketable = false;
     protected bool equipped = false;
     protected Rigidbody2D rb;
     private Coroutine placingCoroutine;
+    private void Show(Transform objTransform)
+    {
+        Renderer renderer = objTransform.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = true;  // Makes the GameObject invisible
+        }
 
+        // Recursively do the same for all children
+        foreach (Transform child in objTransform)
+        {
+            Show(child);  // Recursive call to handle all children
+        }
+    }
+    private void Hide(Transform objTransform)
+    {
+        Renderer renderer = objTransform.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.enabled = false;  // Makes the GameObject invisible
+        }
+
+        // Recursively do the same for all children
+        foreach (Transform child in objTransform)
+        {
+            Hide(child);  // Recursive call to handle all children
+        }
+    }
     public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ItemStart();
     }
+    protected abstract void ItemStart();
     public void Update()
     {
-        if (equipped || !held)
+        if (equipped || !held || pocketable)
         {
             if (active)
                 UpdateItem();
@@ -34,6 +65,13 @@ public abstract class Item : MonoBehaviour
     public void EquipItem()
     {
         gameObject.SetActive(true);
+        Show(transform);
+    }
+    public void PocketItem()
+    {
+        if (!pocketable)
+            gameObject.SetActive(true);
+        Hide(transform);
     }
     protected abstract void UpdateItem();
     public void Place()
